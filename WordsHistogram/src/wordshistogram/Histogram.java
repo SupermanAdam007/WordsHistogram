75dd6713a9d27ac0d5ff492e6b44d5e75504771f
Adam <adam.pavlat@gmail.com>
2017-02-28 22:57:05 +0100

    Complete: words frequencies per file

--- WordsHistogram/src/wordshistogram/Histogram.java
+++ WordsHistogram/src/wordshistogram/Histogram.java
@@ -1,10 +1,18 @@
 package wordshistogram;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
  *
@@ -12,12 +20,15 @@ import java.util.HashMap;
  */
 public class Histogram {
 
-    private HashMap<String, Integer> hist;
+    private final HashMap<String, Integer> hist;
+    private final TreeMap<String, Integer> histSorted;
     private File f;
 
     public Histogram(File f) {
         this.f = f;
         this.hist = new HashMap<>();
+        ValueComparator bvc = new ValueComparator(hist);
+        histSorted = new TreeMap<>(bvc);
         makeHist();
     }
 
@@ -30,32 +41,88 @@ public class Histogram {
             Integer freq;
             while ((word = buffr.readLine()) != null) {
                 split = word.split("\\W+");
-                freq = hist.get(word);
-                if (freq == null) {
-                    hist.put(word, 1);
-                } else {
-                    hist.put(word, freq + 1);
+                for (String s : split) {
+                    freq = hist.get(s);
+                    if (freq == null) {
+                        hist.put(s.toUpperCase(), 1);
+                    } else {
+                        hist.put(s.toUpperCase(), freq + 1);
+                    }
                 }
             }
+
+            histSorted.putAll(hist);
+//            System.out.println("ssssssssssssssssssssssss = " + histSorted.size());
+//            System.out.println(histSorted.toString().replace(" ", "").replace(",", "\n").replace("=", " = "));
+//            System.out.println("sssssssssssssssssssssssss = " + hist.size());
+//            System.out.println(hist.toString().replace(" ", "").replace(",", "\n").replace("=", " = "));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void printRes() {
-        System.out.println(resToString());
-    }
-
     public String resToString() {
+        System.out.println("");
         String textRes = "Histogram for file: " + f.getAbsolutePath() + ":\n";
-        for (String poss : hist.keySet()) {
-            String value = String.valueOf(hist.get(poss));
-            textRes += "      " + poss + " " + value + "\n";
+        for (String poss : histSorted.keySet()) {
+            String value = String.valueOf(histSorted.get(poss));
+            textRes += poss + " " + value + "\n";
         }
         return textRes;
     }
 
+    public String printRange(int from, int to) {
+        System.out.println("");
+        String textRes = "Histogram for file: " + f.getAbsolutePath() + ":\n";
+        for (String poss : hist.keySet()) {
+            String value = String.valueOf(hist.get(poss));
+            if (Integer.valueOf(value) >= from && Integer.valueOf(value) <= to) {
+                textRes += poss + " " + value + "\n";
+            }
+        }
+        return textRes;
+    }
+
+    public void saveHistToFile(int from, int to) {
+        try (FileWriter fw = new FileWriter(f.getAbsolutePath().split("\\.")[0] + "_wordFreq.txt", true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw)) {
+            Set set = histSorted.entrySet();
+            Iterator iterator = set.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry mentry = (Map.Entry) iterator.next();
+//                System.out.print("key is: " + mentry.getKey() + " & Value is: ");
+//                System.out.println(mentry.getValue());
+                if (mentry.getKey().toString().length() >= 4
+                        && Integer.valueOf(String.valueOf((int) mentry.getValue())) >= from
+                        && Integer.valueOf(String.valueOf((int) mentry.getValue())) <= to) {
+                    out.write(mentry.getKey().toString() + " " + mentry.getValue() + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+        }
+    }
+
     public HashMap<String, Integer> getHist() {
         return hist;
+    }
+
+    class ValueComparator implements Comparator<String> {
+
+        Map<String, Integer> base;
+
+        public ValueComparator(Map<String, Integer> base) {
+            this.base = base;
+        }
+
+        @Override
+        public int compare(String a, String b) {
+            if (base.get(a) >= base.get(b)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
     }
 }
--- WordsHistogram/src/wordshistogram/WordsHistogram.java
+++ WordsHistogram/src/wordshistogram/WordsHistogram.java
@@ -12,27 +12,39 @@ public class WordsHistogram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if (args.length == 0) {
+        long startTime = System.currentTimeMillis();
+
+        /*if (args.length == 0) {
             System.out.println("You must enter a path to dir as argument...");
             return;
         }
 
-        System.out.println("Path to dir: " + args[0]);
-
+        System.out.println("Path to dir: " + args[0]);*/
         try {
-            File dir = new File(args[0]);
+//            File dir = new File(args[0]);
+            File dir = new File("C:\\Users\\Adam\\Documents\\NetBeansProjects\\WordsHistogram\\WordsHistogram\\files");
             if (dir.isDirectory()) {
                 for (File f : dir.listFiles()) {
                     if (f.isFile()) {
                         System.out.println("file: " + f);
                         Histogram hist = new Histogram(f);
-                        hist.printRes();
+                        hist.saveHistToFile(0, 999);
+                        
+                        /*if (args[1].length() < 2) {
+                            System.out.println("From 0 to 999: ");
+                            System.out.println(hist.printRange(0, 999));
+                        } else {
+                            System.out.println("From " + args[1] + " to " + args[2] + ": ");
+                            System.out.println(hist.printRange(Integer.valueOf(args[1]), Integer.valueOf(args[2])));
+                        }*/
                     }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println("In time: " + (System.currentTimeMillis() - startTime) + " milis");
     }
 
 }
